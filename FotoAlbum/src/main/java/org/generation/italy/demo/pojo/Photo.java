66507083305a147993fbb18af5ee8f2a7494a8d5
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-20 12:28:54 +0100

    feat: add Category ecosystem + relation MtoM between photo and category
    Category ecosystem (pojo, repo, service)
    - add Category pojo (id, name)
    - create repository interface
    - create service with methods for: save, find all, find by id, delete by id
    
    - add relation Many to Many between photo and category
    - PhotoService add transactional annotation and lazily load collections

--- FotoAlbum/src/main/java/org/generation/italy/demo/pojo/Photo.java
+++ FotoAlbum/src/main/java/org/generation/italy/demo/pojo/Photo.java
@@ -1,6 +1,10 @@
 package org.generation.italy.demo.pojo;
 
+import java.util.List;
+
 import org.hibernate.validator.constraints.URL;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -40,14 +45,27 @@ public class Photo {
 	@Column
 	@NotNull(message = "la visibilità deve essere impostata")
 	private boolean visible;
+	
+	@ManyToMany
+	@JsonIgnore
+	private List<Category> categories;
 
 	public Photo() { }
-	public Photo(String title, String description, String url, String tag, boolean visible) {
+	public Photo(String title, String description, String url, String tag, boolean visible, List<Category> categories) {
 		setTitle(title);
 		setDescription(description);
 		setUrl(url);
 		setTag(tag);
 		setVisible(visible);
+		setCategories(categories);
+	}
+	public Photo(String title, String description, String url, String tag, boolean visible, Category category) {
+		setTitle(title);
+		setDescription(description);
+		setUrl(url);
+		setTag(tag);
+		setVisible(visible);
+		addCategory(category);
 	}
 
 	public int getId() {
@@ -97,12 +115,30 @@ public class Photo {
 	public void setVisible(boolean visible) {
 		this.visible = visible;
 	}
+	
+	public List<Category> getCategories() {
+		return categories;
+	}
+	
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
 
+	public void addCategory(Category category) {
+		boolean finded = false;
+		for (Category c : getCategories())
+			if (c.getId() == category.getId())
+				finded = true;
+		if(!finded)
+			getCategories().add(category);
+	}
+	
 	@Override
 	public String toString() {
 		return "(" + getId() + ") " + getTitle() + "\n" 
 				+ getDescription() + "\n" 
 				+ getTag() + " - " + isVisible() + "\n" 
-				+ getUrl() + "\n";
+				+ getUrl() + "\n \t"
+						+ getCategories();
 	}
 }
