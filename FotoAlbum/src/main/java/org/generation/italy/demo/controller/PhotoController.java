9fdb2ee7a3a3f11a9b40861d0cda1f57bf8f7936
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-20 15:13:10 +0100

    feat: add Photo Controller + index view
    - add PhotoController, with request mapping "/photo" and a method to find all photos for the index
    - add view page html to show the index
    
    - hide spring security dependencies
    - add header navbar in frag.html

---
+++ FotoAlbum/src/main/java/org/generation/italy/demo/controller/PhotoController.java
@@ -0,0 +1,27 @@
+package org.generation.italy.demo.controller;
+
+import java.util.List;
+
+import org.generation.italy.demo.pojo.Photo;
+import org.generation.italy.demo.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/photo")
+public class PhotoController {
+
+	@Autowired
+	private PhotoService photoService;
+
+	@GetMapping("")
+	public String index(Model model) {
+		List<Photo> photos = photoService.findAll();
+		model.addAttribute("photos", photos);
+		return "photo-index";
+	}
+}
